451e2b6739d362668516cfd8630e86caf9001d07
not oleg <104638867+fruitOleg@users.noreply.github.com>
2023-05-29 15:11:34 +0200

    upd task 2

--- js/task-02.js
+++ js/task-02.js
@@ -2,20 +2,29 @@ const ingredients = [
   "Potatoes",
   "Mushrooms",
   "Garlic",
-  "Tomatos",
+  "Tomatoes",
   "Herbs",
   "Condiments",
 ];
 
 const list = document.querySelector("#ingredients");
-const arr = [];
+const elements = ingredients.map(
+  ingredient =>
+    `<li> ${ingredient}</li>`
+);
 
-for (let i = 0; i < ingredients.length; i += 1) {
-  const newItem = document.createElement("li");
-  newItem.textContent = ingredients[i];
-  newItem.classList.add("item");
-  console.log(newItem);
-  arr.push(newItem);
-}
+list.insertAdjacentHTML("beforeend", elements.join(""));
 
-list.append(...arr);
+
+// 
+// const arr = [];
+// 
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const newItem = document.createElement("li");
+//   newItem.textContent = ingredients[i];
+//   newItem.classList.add("item");
+//   console.log(newItem);
+//   arr.push(newItem);
+// }
+// 
+// list.append(...arr);
