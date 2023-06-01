466b9df63fd0cc1758d09b65609dc791fb2e2202
not oleg <104638867+fruitOleg@users.noreply.github.com>
2023-06-01 13:12:45 +0200

    upd

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
 const itemElm = document.querySelectorAll(".item");
-console.log(`Number of categories: ${Number(itemElm.length)}`);
+console.log(`Number of categories:`, itemElm.length);
 
 itemElm.forEach((item) => {
   console.log(`Category: ${item.firstElementChild.textContent}`);
-  console.log(`Elements: `.item.lastElementChild.children.length);
+  console.log(`Elements: `, item.lastElementChild.children.length);
 });
