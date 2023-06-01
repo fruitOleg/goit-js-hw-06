127986fcc435191978403e8b03f3f1554e4f45fb
not oleg <104638867+fruitOleg@users.noreply.github.com>
2023-06-01 13:07:31 +0200

    upd

--- js/task-01.js
+++ js/task-01.js
@@ -3,5 +3,5 @@ console.log(`Number of categories: ${Number(itemElm.length)}`);
 
 itemElm.forEach((item) => {
   console.log(`Category: ${item.firstElementChild.textContent}`);
-  console.log(`Elements: ${Number(item.lastElementChild.children.length)}`);
+  console.log(`Elements: `.item.lastElementChild.children.length);
 });
